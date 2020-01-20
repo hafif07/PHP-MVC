b85501b80f93c10cce0059b11b2cca77ac5b6a66
hafif07 <mhafif29@gmail.com>
2020-01-20 10:55:20 +0700

    menambahkan routing

--- app/core/App.php
+++ app/core/App.php
@@ -6,7 +6,23 @@ class App {
 	public function __construct()
 	{
 
-		echo "Halaman App";
-		
+		$url = $this->parseURL();
+		echo __FILE__."->";
+		var_dump($url);
+
+	}
+
+	public function parseURL()
+	{
+		if(isset($_GET["url"])){
+			//menghilangkan tanda slash di akhir url
+			$url = rtrim($_GET["url"],"/");
+			// bersihkan url dari karakter aneh
+			$url = filter_var($url, FILTER_SANITIZE_URL);
+			// hilangkan slash nya di tiap page routing hom/about/dsb
+			$url = explode("/", $url);
+			return $url;
+		}
+
 	}
 }
